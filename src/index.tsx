1f636ada7e3f3b7d35f45bfabeb397804d6f344f
sean <sean@gomicorp.com>
2021-08-09 01:13:24 +0900

    [chores]  ESLint, tsconfig 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import '@/index.css';
+
 import ReactDOM from 'react-dom';
 
-import { App } from '@src/component';
-import '@src/index.css';
+import { App } from '@/component';
 
 ReactDOM.render(<App />, document.getElementById('root'));
